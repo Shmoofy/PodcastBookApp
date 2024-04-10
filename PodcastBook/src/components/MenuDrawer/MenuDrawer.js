cbc3a82385986eab803a3cf2f2c6a82305b0fe23
FabioKok <fabiokch1@outlook.com>
2024-04-10 18:24:27 -0400

    update menuDrawer.js
    fixed drawer logic, now it shows only the remaining components available to click, and never shows the previously selected podcastdetails page (gave an error and crashed program)

--- PodcastBook/src/components/MenuDrawer/MenuDrawer.js
+++ PodcastBook/src/components/MenuDrawer/MenuDrawer.js
@@ -1,44 +1,45 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import { IconButton } from 'react-native-paper';
-
 
 import SettingsScreen from '../../screens/SettingsScreen';
 import AboutScreen from '../../screens/AboutScreen';
 import HomeScreen from '../../screens/HomeScreen';
-import { useNavigation } from '@react-navigation/native';
 import PodcastDetails from '../../screens/PodcastDetailsScreen/PodcastDetailsScreen';
 
 const Drawer = createDrawerNavigator();
 
 const CustomDrawerContent = ({ navigation, state }) => {
+  const currentRouteName = state.routes[state.index].name;
+
   return (
     <DrawerContentScrollView>
-      {state.routes.map((route) => (
-        <DrawerItem
-          key={route.key}
-          label={route.name}
-          onPress={() => navigation.navigate(route.name)}
-          icon={({ color, size }) => (
-            <IconButton
-              icon="arrow-left"
-              color={color}
-              size={size}
-              onPress={() => {}}
+      {state.routes.map((route) => {
+        // Check if the route name is not the same as the current screen name
+        if (route.name !== currentRouteName && route.name !== 'PodcastDetails') {
+          return (
+            <DrawerItem
+              key={route.key}
+              label={route.name}
+              onPress={() => navigation.navigate(route.name)}
+              icon={({ color, size }) => (
+                <IconButton
+                  icon="arrow-left"
+                  color={color}
+                  size={size}
+                  onPress={() => {}}
+                />
+              )}
             />
-          )}
-        />
-      ))}
+          );
+        }
+        return null; // Skip rendering the current screen in the drawer
+      })}
     </DrawerContentScrollView>
   );
 };
 
 const MenuDrawer = () => {
-    const navigation = useNavigation();
-    useEffect(()=>{
-        navigation.navigate("Home")
-    },[]);
-
   return (
     <Drawer.Navigator drawerContent={({ navigation, state }) => <CustomDrawerContent navigation={navigation} state={state} />}>
         <Drawer.Screen name="Home" component={HomeScreen} />
